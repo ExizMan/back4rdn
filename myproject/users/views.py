47b7a8d42d5c9452b3e15bf2cb4a25ad0d3416dd
ExizMan <yurasankin@yandex.ru>
2023-12-04 16:44:44 +0300

    try

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -4,23 +4,47 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
 from .serializer import UserRegisterSerializer, LoginSerializer, UserHeaderSerializer
+from .models import User, OneTimePassword
 
 
 class RegisterView(GenericAPIView):
     serializer_class = UserRegisterSerializer
 
-    def post(self, request):
+    def post(self, request, commit=False):
         user = request.data
         serializer = self.serializer_class(data=user)
+
         if serializer.is_valid(raise_exception=True):
-            serializer.save()
+            if commit:
+                serializer.save()
+                user_data = serializer.data
+                return Response({
+                    'data': user_data,
+                    'message': 'thanks for signing up'
+                }, status=status.HTTP_201_CREATED)
             user_data = serializer.data
             return Response({
                 'data': user_data,
-                'message': 'thanks for signing up'
-            }, status=status.HTTP_201_CREATED)
+                'message': 'waiting otp'
+            }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class VerifyUserEmail(GenericAPIView):
+    def post(self, request):
+        try:
+            passcode = request.data.get('otp')
+            user_pass_obj=OneTimePassword.objects.get(otp=passcode)
+            user=user_pass_obj.user
+            if not user.is_verified:
+                user.is_verified=True
+                user.save()
+                return Response({
+                    'message':'account email verified successfully'
+                }, status=status.HTTP_200_OK)
+            return Response({'message':'passcode is invalid user is already verified'}, status=status.HTTP_204_NO_CONTENT)
+        except OneTimePassword.DoesNotExist as identifier:
+            return Response({'message':'passcode not provided'}, status=status.HTTP_400_BAD_REQUEST)
 
 class LoginUserView(GenericAPIView):
     serializer_class = LoginSerializer
